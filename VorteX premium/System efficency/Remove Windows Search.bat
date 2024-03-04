00b1533a07946202cf6a83e4bb1bc518c569bf90
FardinXS <145196020+FardinXS@users.noreply.github.com>
2024-03-04 14:45:07 -0800

    Delete VorteX premium directory

--- VorteX premium/System efficency/Remove Windows Search.bat
+++
@@ -1,5 +0,0 @@
-rem Disable Search (Cortana/to restore run SFC scan)
-winget uninstall "cortana"
-takeown /s %computername% /u %username% /f "%WINDIR%\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe"
-icacls "%WINDIR%\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" /inheritance:r /grant:r %username%:F
-ren "%WINDIR%\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" "SearchHost.exe.bak"
